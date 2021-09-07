d1d08c6e70ba2bece3295f67440078ba7b9e53d7
Stan <st@Stans-MacBook-Pro.local>
2021-09-07 23:33:08 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,125 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import "./App.css";
+import miscibleVideo from "./video/miscible.mp4";
+import miscible from "./images/miscibleBG.jpg";
+import logoGrey from "./images/logo_grey.png";
+import logoGradient from "./images/logo_gradient1.png";
+
+import heels1 from "./images/RR_Rombaut_Heels_1.jpg";
+import heels2 from "./images/RR_Rombaut_Heels_3.jpg";
+import heels3 from "./images/RR_Rombaut_Heels_4.jpg";
+
+import luxus1 from "./images/luxus1.jpg";
+import luxus2 from "./images/luxus2.jpg";
+import luxus3 from "./images/luxus3.jpg";
+import luxus4 from "./images/luxus4.jpg";
+import luxus5 from "./images/luxus5.jpg";
+import luxus6 from "./images/luxus6.jpg";
+
+import rest from "./images/REST_cover.jpg";
+import rest1 from "./images/REST1.jpg";
+import rest2 from "./images/REST2.jpg";
+import rest3 from "./images/REST3.jpg";
+import rest4 from "./images/REST4.jpg";
+import rest5 from "./images/REST5.jpg";
+import rest6 from "./images/REST6.jpg";
+
+import ambivalence from "./images/ambivalenceBG.jpg";
+import ambivalence1 from "./images/ambivalence01.jpg";
+import ambivalence2 from "./images/ambivalence02.jpg";
+import ambivalence3 from "./images/ambivalence03.jpg";
+import ambivalence4 from "./images/ambivalence04.jpg";
+import ambivalence5 from "./images/ambivalence05.jpg";
+
+import selective from "./video/selective.mp4";
+
+import { Work } from "./components/Work";
 
 function App() {
+  const [logo, setLogo] = useState(logoGrey);
+
   return (
     <div className="App">
-      <header className="App-header">
-        this is the header
-      </header>
+      <div className="left">
+        <div
+          className="logo"
+          onMouseOver={() => setLogo(logoGradient)}
+          onMouseLeave={() => setLogo(logoGrey)}
+        >
+          <img src={logo}></img>
+        </div>
+        <div className="shop">
+          SHOP
+        </div>
+      </div>
+      <div className="middle">
+        <Work
+          video={selective}
+          header="Selective Memory"
+          content="Part of group show « Casting the Runes » curated by Arthur Poujois"
+          location="2021 - London, United Kingdom"
+        />
+        <Work
+          images={[rest, rest1, rest2, rest3, rest4, rest5, rest6]}
+          header="Rest"
+          content="Print design and production development for Michele Rizzo"
+          location="2020 - Rome, Italy"
+        />
+        <Work
+          images={[luxus6, luxus1, luxus2, luxus3, luxus4, luxus5]}
+          header="Luxus für Alle "
+          content="Print design and development for Annemarie Šarić"
+          location="2020 - Antwerp, Belgium"
+        />
+        <Work
+          images={[heels1, heels2, heels3]}
+          header="DYSMORPHIA Heels"
+          content="Design development and production follow up for ROMBAUT"
+          location="2019 - Paris, France"
+        />
+        <Work
+          img={miscible}
+          header="Ambivalence"
+          content="Visuals on holographic display: Sam Rolfes"
+          location="2018 - Antwerp, Belgium"
+        />
+        <Work
+          images={[
+            ambivalence,
+            ambivalence1,
+            ambivalence2,
+            ambivalence3,
+            ambivalence4,
+            ambivalence5,
+          ]}
+          header="Miscible Displacement"
+          content="Bachelor collection"
+          location="2016 - Antwerp, Belgium"
+        />
+        <video
+          className="video"
+          src={miscibleVideo}
+          type="video/mp4"
+          autoPlay
+          loop
+          muted
+        />
+      </div>
+      <div className="right">
+        <div className="contact">
+          <p>kartchev@gmail.com</p>
+          <p>@stefankartchev</p>
+          <p>( + 4 9 ) 0176 99 66 66 66</p>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Work.js
@@ -0,0 +1,52 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/swiper-bundle.css";
+
+export const Work = ({ img, images, header, content, location, video }) => {
+
+  return (
+    <div className="container">
+      {video ? (
+        <video
+          className="video"
+          src={video}
+          type="video/mp4"
+          autoPlay
+          loop
+          muted
+        />
+      ) : null}
+      {img ? <img alt="miscible" src={img} objectFit></img> : null}
+      {images ? (
+        <div className="swip-container">
+          <Swiper
+            spaceBetween={50}
+            slidesPerView={1}
+            onSwiper={(swiper) => console.log(swiper)}
+            onSlideChange={() => console.log('slide change')}
+          >
+            {images.map((i, index) => (
+              <SwiperSlide>
+                <img key={index} src={i}></img>
+              </SwiperSlide>
+            ))}
+          </Swiper>{" "}
+        </div>
+      ) : null
+      }
+      <div className="topright">
+        <div className="header" style={{ textAlign: "right" }}>
+          {header}
+        </div>
+        <div className="content" style={{ textAlign: "right" }}>
+          {content}
+        </div>
+        <div className="content" style={{ textAlign: "right" }}>
+          {location}
+        </div>
+      </div>
+    </div >
+  );
+};
