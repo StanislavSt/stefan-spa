0b78d59ee5ed3c2646a964250821fcf2a37c7fb4
Stanislav Staev <s.staev1@gmail.com>
2021-12-06 19:16:24 +0100

    add snipcart API functionality

--- src/components/MyShop.js
+++ src/components/MyShop.js
@@ -1,22 +1,36 @@
-import videoItem from '../video/karthecv-campaign.webm';
-import { useEffect, useRef } from 'react';
-import { Product } from './Product'
+import videoItem from "../video/karthecv-campaign.webm";
+import { useEffect, useRef } from "react";
+import { Product } from "./Product";
+import products from "../products.json";
+
 export default function MyShop() {
-    const vidRef = useRef(null)
-    const handleOnClick = () => {
-        vidRef.current.paused ? vidRef.current.play() : vidRef.current.pause()
-        vidRef.current.muted = false
-    }
-    useEffect(() => {
-        console.log(vidRef)
-        vidRef.current.play()
-    }, [])
-    return (<div>
-        <div className="big-image" style={{ cursor: "pointer" }}>
-            <video ref={vidRef} loop onClick={() => handleOnClick()} autoplay muted playsinline >
-                <source src={videoItem} />
-            </video>
-        </div>
-        <Product />
-    </div>)
+  const vidRef = useRef(null);
+  const handleOnClick = () => {
+    vidRef.current.paused ? vidRef.current.play() : vidRef.current.pause();
+    vidRef.current.muted = false;
+  };
+  useEffect(() => {
+    console.log(vidRef);
+    vidRef.current.play();
+  }, []);
+  console.log(products)
+  return (
+    <div>
+      <div className="big-image" style={{ cursor: "pointer" }}>
+        <video
+          ref={vidRef}
+          loop
+          onClick={() => handleOnClick()}
+          autoplay
+          muted
+          playsinline
+        >
+          <source src={videoItem} />
+        </video>
+      </div>
+      {products.map((product) => (
+        <Product pp={product} id={product.id} />
+      ))}
+    </div>
+  );
 }
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,32 +1,98 @@
 import React from "react";
-import products from '../products.json';
+import { useEffect, useState } from "react";
 
-export const Product = () => {
+export const Product = ({ pp, id }) => {
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [products, setProducts] = useState(null);
+  const [selected, setSelected] = useState("Size");
 
-    return (<div >
-        {products.map(product => {
-            return (
-                <div key={product.id} >
-                    <img src={product.image} alt={`Preview of ${product.title}`} />
-                    <h3>{product.title}</h3>
-                    <p>{product.description}</p>
-                    <p>€{product.price}</p>
-                    <p>
-                        <button className="snipcart-add-item"
-                            data-item-url="/"
-                            data-item-id={product.id}
-                            data-item-image={product.image}
-                            data-item-name={product.title}
-                            data-item-price={product.price}
-                            data-item-custom1-name="Size"
-                            data-item-custom1-options="Small|Medium|Large"
-                            data-item-custom1-value="Medium"
+  const handleOnClick = (e) => {
+    setSelected(e.target.textContent);
+    setShowDropdown(false);
+  };
+  const secret = process.env.REACT_APP_SNIPCART_API;
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const request = await fetch(
+        `https://app.snipcart.com/api/products/${id}`,
+        {
+          headers: {
+            Authorization: `Basic ${btoa(secret)}`,
+            Accept: "application/json",
+          },
+        }
+      );
+      const result = await request.json();
+      setProducts(result);
+    };
+    getProducts();
+  }, []);
+  return (
+    <div key={pp.id}>
+      <div className="product-container">
+        <img
+          src={pp.image}
+          alt={`Preview of ${pp.title}`}
+          className="product-image"
+        />
+        <div className="product-description">
+          <h3>{pp.title}</h3>
+          <p>{pp.description}</p>
+          <p>€{pp.price}</p>
+
+          <div className="dropdown-container">
+            <div
+              className="dropdown-header"
+              onClick={() => setShowDropdown(!showDropdown)}
+            >
+              <span>{selected}</span>
+            </div>
+            {showDropdown ? (
+              <span className="select-box--arrow-up"></span>
+            ) : (
+              <span className="select-box--arrow-down"></span>
+            )}
+
+            {showDropdown ? (
+              <div className="dropdown-list">
+                {products
+                  ? products.variants.map((x) => {
+                      return (
+                        <div
+                          onClick={handleOnClick}
+                          className={x.stock <= 0 ? "soldOut" : null}
                         >
-                            Add to Cart
-                        </button>
-                    </p>
-                </div>
-            );
-        })}
-    </div>)
-}
+                          {x.variation[0].option}
+                        </div>
+                      );
+                    })
+                  : null}
+              </div>
+            ) : null}
+          </div>
+
+          <p>
+            <button
+              className={
+                selected == "Size"
+                  ? "snipcart-add-item selectSize"
+                  : "snipcart-add-item"
+              }
+              data-item-url="https://stefankartchev.netlify.app/products.json"
+              data-item-id={pp.id}
+              data-item-image={pp.image}
+              data-item-name={pp.title}
+              data-item-price={pp.price}
+              data-item-custom1-name="Size"
+              data-item-custom1-options="Small|Medium|Large"
+              data-item-custom1-value={selected}
+            >
+              Add to Cart
+            </button>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
